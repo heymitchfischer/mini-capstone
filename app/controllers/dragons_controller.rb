a2d584ea0c793fd7c6702c19e35b59a49c4c82fd
heymitchfischer <mitchfischer6@gmail.com>
2017-09-05 21:26:16 -0500

    added edit, update, and destroy

--- app/controllers/dragons_controller.rb
+++ app/controllers/dragons_controller.rb
@@ -3,7 +3,53 @@ class DragonsController < ApplicationController
     @dragons = Dragon.all
   end
 
+  def new
+    
+  end
+
+  def create
+    dragon = Dragon.new(
+                        name: params[:name],
+                        price: params[:price],
+                        description: params[:description],
+                        image: params[:image],
+                        amount_spikes: params[:amount_spikes],
+                        color: params[:color],
+                        size: params[:size]
+                        )
+    dragon.save
+    flash[:success] = "Dragon successfully created!"
+    redirect_to "/dragons/#{dragon.id}"
+  end
+
   def show
     @dragon = Dragon.find(params[:id])
+  end
+
+  def edit
+    @dragon = Dragon.find(params[:id])
+  end
+
+  def update
+    dragon = Dragon.find(params[:id])
+    dragon.assign_attributes(
+                              name: params[:name],
+                              price: params[:price],
+                              description: params[:description],
+                              image: params[:image],
+                              amount_spikes: params[:amount_spikes],
+                              color: params[:color],
+                              size: params[:size]
+                              )
+    dragon.save
+    flash[:info] = "Dragon successfully updated"
+    redirect_to "/dragons/#{dragon.id}"
+  end
+
+  def destroy
+    dragon = Dragon.find(params[:id])
+    dragon.destroy
+    flash[:warning] = "Dragon successfully destroyed"
+    redirect_to "/dragons"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   get '/' => 'dragons#index'
   get '/dragons' => 'dragons#index'
+
+  get '/dragons/new' => 'dragons#new'
+  post '/dragons' => 'dragons#create'
+
   get '/dragons/:id' => 'dragons#show'
+
+  get '/dragons/:id/edit' => 'dragons#edit'
+  patch '/dragons/:id' => 'dragons#update'
+
+  delete '/dragons/:id/' => 'dragons#destroy'
 end

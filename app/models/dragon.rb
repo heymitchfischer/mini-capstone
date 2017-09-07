bb224ffc06cc3545d01b12e2a268e8b766956da9
heymitchfischer <mitchfischer6@gmail.com>
2017-09-06 21:33:56 -0500

    added Bootstrap columns, etc

--- app/models/dragon.rb
+++ app/models/dragon.rb
@@ -1,2 +1,14 @@
 class Dragon < ApplicationRecord
+  def sale_message
+    return "Discount Item!" if price < 2
+    return "Everyday Value!" if price >= 2
+  end
+
+  def tax
+    return price*0.09
+  end
+
+  def total
+    return self.tax + price
+  end
 end
